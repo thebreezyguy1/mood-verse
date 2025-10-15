8c8acf4b6469d4ea54609d9d18979b905d50da14
Dorian Taponzing Donfack <doriantaponzing@gmail.com>
2025-10-15 00:48:25 -0400

    feat: added profile image in header

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 type headerProps = {
   title: string;
@@ -7,11 +7,19 @@ type headerProps = {
 const Header = (props: headerProps) => {
   return (
     <View style={styles.container}>
-      <Image />
-      <Text style={{ fontSize: 20, fontWeight: "700" }}>{props.title}</Text>
+      <TouchableOpacity>
+        <Image
+          style={styles.profileImg}
+          source={require("../../assets/images/dorian-img.jpg")}
+        />
+      </TouchableOpacity>
+
       <View>
-        <Image />
-        <Text>{new Date().toDateString()}</Text>
+        <Text style={{ fontSize: 20, fontWeight: "700" }}>{props.title}</Text>
+        <View>
+          <Image />
+          <Text>{new Date().toDateString()}</Text>
+        </View>
       </View>
     </View>
   );
@@ -21,11 +29,19 @@ const styles = StyleSheet.create({
   container: {
     height: 100,
     width: "100%",
-    flexDirection: "column",
+    flexDirection: "row",
+    alignItems: "center",
     backgroundColor: "pink",
-    justifyContent: "center",
+    justifyContent: "flex-start",
     paddingHorizontal: 20,
-    gap: 5,
+    gap: 10,
+  },
+  profileImg: {
+    width: 50,
+    height: 50,
+    borderRadius: "50%",
+    borderWidth: 2,
+    borderColor: "#fff",
   },
 });
 
