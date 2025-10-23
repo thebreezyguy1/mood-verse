db2422e96b99a9f5587d2d2d656f01e65aa8cff6
Dorian Taponzing Donfack <doriantaponzing@gmail.com>
2025-10-22 23:43:47 -0400

    feat: updated style on profile screen

--- app/profile.tsx
+++ app/profile.tsx
@@ -1,3 +1,4 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { useRouter } from "expo-router";
 import React from "react";
 import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
@@ -5,17 +6,43 @@ import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 export default function ProfileScreen() {
   const router = useRouter();
 
+  const user = {
+    firstName: "Dorian",
+    lastName: "Taponzing Donfack",
+    email: "doriantaponzing@gmail.com",
+    phoneNumber: "+1(470)439-9907",
+    address: "3920 Snipes Court, Lilburn, GA",
+  };
+
   return (
     <View style={styles.container}>
       <TouchableOpacity onPress={() => router.back()} style={styles.backButton}>
-        <Text style={{ color: "#fff" }}>Back</Text>
+        <Ionicons name="arrow-back-outline" size={24} color="black" />
       </TouchableOpacity>
-      <Image
-        source={require("../assets/images/dorian-img.jpg")}
-        style={styles.avatar}
-      />
-      <Text style={styles.name}>Dorian</Text>
-      <Text style={styles.bio}>Mood enthusiast. Building vibes.</Text>
+      <View style={styles.profileHeader}>
+        <Image
+          source={require("../assets/images/dorian-img.jpg")}
+          style={styles.avatar}
+        />
+        <View>
+          <Text style={styles.name}>
+            {user.firstName + " " + user.lastName}
+          </Text>
+          <Text style={styles.bio}>{user.email}</Text>
+        </View>
+      </View>
+      <View style={styles.profileContainer}>
+        <View style={styles.profileInfo}>
+          <Text style={styles.title}>Email</Text>
+          <Text>{user.email}</Text>
+        </View>
+      </View>
+      <View>
+        <View style={styles.profileInfo}>
+          <Text style={styles.title}>Address</Text>
+          <Text>{user.address}</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -23,34 +50,46 @@ export default function ProfileScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#fff",
+    backgroundColor: "pink",
     padding: 20,
   },
   backButton: {
-    position: "absolute",
-    top: 50,
-    left: 20,
-    backgroundColor: "#FF69B4",
     padding: 8,
     borderRadius: 8,
   },
   avatar: {
-    width: 140,
-    height: 140,
+    width: 70,
+    height: 70,
     borderRadius: 70,
     borderWidth: 3,
-    borderColor: "#FF69B4",
-    marginBottom: 20,
+    borderColor: "#fff",
   },
   name: {
-    fontSize: 24,
+    fontSize: 20,
     fontWeight: "700",
+    color: "#fff",
   },
   bio: {
-    marginTop: 8,
-    color: "#666",
+    marginTop: 2,
+    color: "#fff",
     textAlign: "center",
+  },
+  profileHeader: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+    marginTop: 20,
+  },
+  profileContainer: {
+    marginTop: 20,
+  },
+  profileInfo: {
+    backgroundColor: "#fff",
+    marginBottom: 20,
+    padding: 10,
+    borderRadius: 10,
+  },
+  title: {
+    fontWeight: "700",
   },
 });
